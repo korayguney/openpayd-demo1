f5485b899580293053c55116fb0a7bbeece8d125
Koray Guney <koray.guney@hotmail.com>
2021-07-04 12:21:31 +0300

    updated tests

--- src/test/java/com/openpayd/currencyconverter/controllers/CurrencyConverterControllerTest.java
+++ src/test/java/com/openpayd/currencyconverter/controllers/CurrencyConverterControllerTest.java
@@ -28,30 +28,36 @@ class CurrencyConverterControllerTest {
     CurrencyConverterController converterController;
 
     @Test
-    void convertCurrencies() {
+    void should_ReturnEquals_When_convertedValuesEqual() {
+        // given
         Optional<Double> expected = Optional.of(1500.15);
         when(mockCurrencyService.convert(any())).thenReturn(expected);
 
+        // when
         ConversionCurrency currency = new ConversionCurrency();
         currency.setFrom("USD");
         currency.setTo("TRY");
         currency.setValue(100);
         Double actual = converterController.convertCurrencies(currency).getBody();
 
+        // then
         assertEquals(expected.get(), actual.doubleValue());
     }
 
     @Test
-    void convertCurrenciesBadRequest() {
+    void should_ReturnFalse_When_convertResultIsEmpty() {
+        // given
         Optional<Double> expected = Optional.empty();
         when(mockCurrencyService.convert(any())).thenReturn(expected);
 
+        // when
         ConversionCurrency currency = new ConversionCurrency();
         currency.setFrom("USD");
         currency.setTo("TRY");
         currency.setValue(100);
         ResponseEntity<Double> actual = converterController.convertCurrencies(currency);
 
+        // then
         assertAll(
                 () -> assertFalse(expected.isPresent()),
                 () -> assertEquals(HttpStatus.BAD_REQUEST, actual.getStatusCode())
@@ -59,19 +65,21 @@ class CurrencyConverterControllerTest {
     }
 
     @Test
-    void getAllCurrencies() {
+    void should_ReturnEquals_When_allCurrenciesAreFetched() {
+        // given
         List<Currency> expected = Arrays.asList(
                 new Currency("TRY", 4.5),
                 new Currency("EUR", 2.4),
                 new Currency("USD", 3.1));
         when(mockCurrencyService.getAllCurrencies()).thenReturn(expected);
 
+        // when
         List<Currency> actual = converterController.getAllCurrencies().getBody();
 
+        // then
         assertAll(
                 () -> assertEquals(expected.size(), actual.size()),
                 () -> assertEquals(expected.get(1).getName(), actual.get(1).getName())
         );
     }
-
 }
--- src/test/java/com/openpayd/currencyconverter/services/CurrencyServiceTest.java
+++ src/test/java/com/openpayd/currencyconverter/services/CurrencyServiceTest.java
@@ -2,6 +2,7 @@ package com.openpayd.currencyconverter.services;
 
 import com.openpayd.currencyconverter.exceptions.CurrencyConverterException;
 import com.openpayd.currencyconverter.exceptions.ErrorMessageConstants;
+import com.openpayd.currencyconverter.exceptions.TransactionDateParseException;
 import com.openpayd.currencyconverter.models.ConversionCurrency;
 import com.openpayd.currencyconverter.models.Currency;
 import com.openpayd.currencyconverter.repositories.CurrencyRepository;
@@ -9,9 +10,12 @@ import com.openpayd.currencyconverter.utils.ClientRequestInfo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -31,15 +35,18 @@ class CurrencyServiceTest {
     CurrencyService currencyService;
 
     @Test
-    void getAllCurrencies() {
+    void should_ReturnEquals_When_allCurrenciesAreFetched() {
+        // given
         List<Currency> expected = Arrays.asList(
                 new Currency("TRY", 4.5),
                 new Currency("EUR", 2.4),
                 new Currency("USD", 3.1));
         when(mockCurrencyRepository.findAll()).thenReturn(expected);
 
+        // when
         List<Currency> actual = this.currencyService.getAllCurrencies();
 
+        // then
         assertAll(
                 () -> assertTrue(actual.size() == 3),
                 () -> assertEquals(actual.get(0).getName(), "EUR"),
@@ -49,7 +56,8 @@ class CurrencyServiceTest {
     }
 
     @Test
-    void convert() {
+    void should_ReturnTrue_When_convertResultIsNotEmpty() {
+        // given
         Optional<Currency> expected1 = Optional.of(new Currency("TRY", 4.0));
         when(mockCurrencyRepository.findByName(eq("TRY"))).thenReturn(expected1);
         Optional<Currency> expected2 = Optional.of(new Currency("EUR", 2.0));
@@ -57,8 +65,11 @@ class CurrencyServiceTest {
         when(mockClientRequestInfo.getClientIpAdress()).thenReturn(anyString());
 
         ConversionCurrency conversionCurrency = new ConversionCurrency("TRY", "EUR", 100);
+
+        // when
         Optional<Double> actual = this.currencyService.convert(conversionCurrency);
 
+        // then
         assertAll(
                 () -> assertTrue(actual.isPresent()),
                 () -> assertEquals(50.0, actual.get())
@@ -66,18 +77,53 @@ class CurrencyServiceTest {
     }
 
     @Test
-    void convert2() {
+    void should_throwsCurrencyConverterException_When_conversionCurrenciesAreEmpty() {
+        // given
         ConversionCurrency conversionCurrency = new ConversionCurrency("", "", 100);
+
+        // when
         Executable executable = () -> this.currencyService.convert(conversionCurrency);
+
+        // then
         CurrencyConverterException exp = assertThrows(CurrencyConverterException.class, executable);
         assertTrue(exp.getMessage().equals(ErrorMessageConstants.CURRENCY_CONVERTER_CURRENCIES_EMPTY));
     }
 
     @Test
-    void convert3() {
+    void should_throwsCurrencyConverterException_When_conversionCurrenciesAreIdentical() {
+        // given
         ConversionCurrency conversionCurrency = new ConversionCurrency("EUR", "EUR", 100);
+
+        // when
         Executable executable = () -> this.currencyService.convert(conversionCurrency);
+
+        // then
         CurrencyConverterException exp = assertThrows(CurrencyConverterException.class, executable);
         assertTrue(exp.getMessage().equals(ErrorMessageConstants.CURRENCY_CONVERTER_CURRENCIES_EQUAL));
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {0, -100, -0.5})
+    void should_throwsCurrencyConverterException_When_conversionCurrencyValueIsZeroOrMinus(Double value) {
+        // given
+        ConversionCurrency conversionCurrency = new ConversionCurrency("EUR", "TRY", value);
+
+        // when
+        Executable executable = () -> this.currencyService.convert(conversionCurrency);
+
+        // then
+        CurrencyConverterException exp = assertThrows(CurrencyConverterException.class, executable);
+        assertTrue(exp.getMessage().equals(ErrorMessageConstants.CURRENCY_CONVERTER_AMOUNT_IS_MINUS));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1","21.10.2022",""})
+    void should_throwsTransactionDateParseException_When_transactionDateFormatIsWrong(String transaction_date) {
+        // when
+        Executable executable = () -> this.currencyService.getAllConversions(transaction_date, Pageable.unpaged());
+
+        // then
+        TransactionDateParseException exp = assertThrows(TransactionDateParseException.class, executable);
+        assertTrue(exp.getMessage().equals(ErrorMessageConstants.TRANSACTION_DATE_IS_WRONG));
     }
 }
